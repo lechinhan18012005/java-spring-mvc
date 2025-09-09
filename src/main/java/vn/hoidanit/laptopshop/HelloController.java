1d7b8af0072cfb22ee1455a2ccd97c9f9c493bdb
Nhan Lee <lechinhan18012005@gmail.com>
2025-09-10 00:02:40 +0700

    add security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,15 @@ public class HelloController {
     public String index() {
         return "Hello World from Spring Boot!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can see this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can see this page!";
+    }
+
 }
